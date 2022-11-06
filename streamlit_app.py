712c3b32a524032b95775b6a4275cff4174ba2bf
tenjinspace <tenjinspace@protonmail.com>
2022-11-07 00:29:16 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 
 # Load data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+df = pd.read_csv('/Golden_Ticket_Award_Winners_Wood.csv')
 
-st.title('Solar Employment by Sector, 2010')
+st.title('Roller Coaster Data from Around the World')
 st.markdown("Github Copilot test")
-st.header("Test 1")
 
+st.header("Test 1")
 st.dataframe(df)
 
