8123553e2c05f56270402360cf8d6e5e3f1a93a0
tenjinspace <tenjinspace@protonmail.com>
2022-11-07 00:24:56 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
 
+# Load data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+
+st.title('Solar Employment by Sector, 2010')
+st.dataframe(df)
