99f5696adf767f119b6e382fdd0698b1d945df6e
tenjinspace <tenjinspace@protonmail.com>
2022-11-07 00:31:46 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 
 # Load data from Golden_Ticket_Award_Winners_Wood.csv
-df = pd.read_csv('Golden_Ticket_Award_Winners_Wood.csv')
+#df = pd.read_csv('Golden_Ticket_Award_Winners_Wood.csv')
 
 st.title('Roller Coaster Data from Around the World')
 st.markdown("Github Copilot test")
 
 st.header("Test 1")
-st.dataframe(df)
+#st.dataframe(df)
 
