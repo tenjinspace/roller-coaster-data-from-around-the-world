e31855336e32ccb749abd64c4f52860f4d0f3956
tenjinspace <tenjinspace@protonmail.com>
2022-11-07 00:27:05 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,8 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 
 st.title('Solar Employment by Sector, 2010')
+st.markdown("Github Copilot test")
+st.header("Test 1")
+
 st.dataframe(df)
+
