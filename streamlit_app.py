c13e0643c40373cf1392a78c58b0466569983718
tenjinspace <tenjinspace@protonmail.com>
2022-11-07 00:30:34 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-# Load data
-df = pd.read_csv('/Golden_Ticket_Award_Winners_Wood.csv')
+# Load data from Golden_Ticket_Award_Winners_Wood.csv
+df = pd.read_csv('Golden_Ticket_Award_Winners_Wood.csv')
 
 st.title('Roller Coaster Data from Around the World')
 st.markdown("Github Copilot test")
